a850aea66af8d623e988a805240f8942e3156b46
Think <564603061@qq.com>
2022-02-10 16:58:45 +0800

    新增反射扩展方法和对应的单元测试

--- src/ThinkingHelper/Extensions/ThinkingReflectionExtensions.cs
+++ src/ThinkingHelper/Extensions/ThinkingReflectionExtensions.cs
@@ -2,29 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using ThinkingHelper;
 
-// ReSharper disable once CheckNamespace
-namespace ThinkingHelper.Reflection.Extensions;
+// ReSharper disable CheckNamespace
 
-public static class ThinkingReflectionExtensions
+namespace ThinkingHelper.Reflection.Extensions
 {
-    /// <summary>
-    /// 将一个对象，转为字典表示。只获取对象公开的并具有读取器的实例属性
-    /// </summary>
-    public static Dictionary<string, object?> ToDictionary(this object obj)
+    public static class ThinkingReflectionExtensions
     {
-        return ToDictionary(obj, info => info.GetValue(obj));
-    }
+        /// <summary>
+        /// 将一个对象，转为字典表示。只获取对象公开的并具有读取器的实例属性
+        /// </summary>
+        public static Dictionary<string, object?> ToDictionary(this object obj)
+        {
+            return ToDictionary(obj, info => info.GetValue(obj));
+        }
 
-    /// <summary>
-    /// 将一个对象，转为字典表示。只获取对象公开的并具有读取器的实例属性
-    /// </summary>
-    public static Dictionary<string, TValue> ToDictionary<TValue>(this object obj, Func<PropertyInfo, TValue> elementSelector)
+        /// <summary>
+        /// 将一个对象，转为字典表示。只获取对象公开的并具有读取器的实例属性
+        /// </summary>
+        public static Dictionary<string, TValue> ToDictionary<TValue>(this object obj, Func<PropertyInfo, TValue> elementSelector)
+        {
+            Check.NotNull(obj);
+            Check.NotNull(elementSelector);
+            return obj.GetType()
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(info => info.CanRead)
+                .ToDictionary(info => info.Name, elementSelector);
+        }
+    }
+}
+
+namespace System.Reflection
+{
+    public static class ThinkingReflectionExtensions
     {
-        Check.NotNull(obj);
-        Check.NotNull(elementSelector);
-        return obj.GetType()
-            .GetProperties(BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance)
-            .ToDictionary(info => info.Name, elementSelector);
+        /// <summary>
+        /// Gets a value indicating whether the current type is a anonymous type.
+        /// </summary>
+        /// <returns>true if the current type is a anonymous type; otherwise, false.</returns>
+        public static bool IsAnonymousType(this Type type)
+        {
+            Check.NotNull(type);
+            return Attribute.IsDefined(type, typeof(CompilerGeneratedAttribute))
+                   && type.Namespace == null
+                   && type.IsClass
+                   && !type.IsAbstract
+                   && type.IsNotPublic
+                   && type.IsGenericType
+                   && type.IsSealed
+                   && !type.IsSerializable
+                   && type.BaseType == typeof(object)
+                   && type.Name.Contains("AnonymousType");
+        }
     }
 }
---
+++ test/ThinkingHelper.Test/Extensions/ThinkingReflectionExtensionsTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using ThinkingHelper.Reflection.Extensions;
+using Xunit;
+
+namespace ThinkingHelper.Test.Extensions;
+
+public class TestClass
+{
+    private int _b;
+    public string? A { get; set; }
+
+    public static object? StatA { get; set; }
+
+    public int B
+    {
+        set => _b = value;
+    }
+
+    private DateTime Time { get; set; } = DateTime.Now;
+}
+
+public class ThinkingReflectionExtensionsTest
+{
+    [Fact]
+    public void ToDictionary_ShouldNotThrowException()
+    {
+        string aValue = "aaa";
+        int bValue = 20;
+        TestClass.StatA = new object();
+        var testClass = new TestClass {A = aValue, B = bValue};
+        var dic = testClass.ToDictionary(info => info.GetValue(testClass)?.ToString());
+        Assert.Single(dic);
+        Assert.Equal(dic["A"], aValue);
+    }
+
+    [Fact]
+    public void IsAnonymousType_Type_ShouldBeFalse()
+    {
+        var testClass = new TestClass();
+        bool actual = testClass.GetType().IsAnonymousType();
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void IsAnonymousType_Type_ShouldBeTrue()
+    {
+        var testClass = new {Id = 10};
+        bool actual = testClass.GetType().IsAnonymousType();
+        Assert.True(actual);
+    }
+}
